796bc564fe06ada866480c00b718b255cdb69d82
Athul Rajagopal <athulrajagopal2002@gmail.com>
2020-07-03 20:23:45 -0600

    added very basic Patient Scheduling framework

--- front-end-patient-app/main.js
+++ front-end-patient-app/main.js
@@ -30,7 +30,7 @@ function createWindow() {
     });
 
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'serial.html'),
+        pathname: path.join(__dirname, 'homescreen.html'),
         protocol: 'file',
         slashes: true
     }));
